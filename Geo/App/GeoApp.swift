04b3a5df68245c87b1d94b6fd379fb0c758f6f30
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-28 16:38:46 -0300

    auto: Geo/App/GeoApp.swift

--- Geo/App/GeoApp.swift
+++ Geo/App/GeoApp.swift
@@ -419,6 +419,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         container.notificationManager.stopMonitoring()
         container.watcher.stopWatching()
         container.mcpServer.stop()
+        container.httpServer.stop()
         MainActor.assumeIsolated { container.nanoHermesService.stop() }
     }
 
