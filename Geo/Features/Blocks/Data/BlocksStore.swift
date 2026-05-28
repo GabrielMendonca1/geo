95d70e299d9910f9ab6aed1a864c6b2afc3eb1f6
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-27 23:28:12 -0300

    auto: Geo/Features/Blocks/Data/BlocksStore.swift

--- Geo/Features/Blocks/Data/BlocksStore.swift
+++ Geo/Features/Blocks/Data/BlocksStore.swift
@@ -908,7 +908,7 @@ extension BlocksStore {
     }
 
     func toggleCheckbox(in blockId: String, lineNumber: Int) async throws {
-        guard let block = blocks.first(where: { $0.id == blockId }) else {
+        guard let block = block(withId: blockId) else {
             throw BlockCheckboxError.blockNotFound
         }
         var lines = BlockCheckboxParsing.splitLines(block.markdown)
