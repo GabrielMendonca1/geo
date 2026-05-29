1a459e027e187c7f98c4ee08bc38209a64d6ed06
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-29 09:14:57 -0300

    auto: Geo/App/GeoApp.swift

--- Geo/App/GeoApp.swift
+++ Geo/App/GeoApp.swift
@@ -240,6 +240,7 @@ struct GeoApp: App {
         .commands {
             AboutCommand()
             AlwaysOnTopCommand()
+            CheckForUpdatesCommand(updater: updaterController.updater)
 
             GeoCommands()
         }
