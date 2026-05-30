cf491ea6736d1644a54599dc7c4ec3d1aaca366d
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-30 17:48:39 -0300

    auto: Geo/App/GeoApp.swift

--- Geo/App/GeoApp.swift
+++ Geo/App/GeoApp.swift
@@ -316,6 +316,7 @@ final class BlockEditorActionsHolder: ObservableObject {
 class AppDelegate: NSObject, NSApplicationDelegate {
     private let container = AppContainer.live
     private var permissionStateCancellable: AnyCancellable?
+    private var hitTestMonitor: Any?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         PerformanceTracker.shared.markStartupEnd()
