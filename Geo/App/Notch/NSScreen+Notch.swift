54784b559fefca24a3abcc5a240eb80c91186b81
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-30 13:23:09 -0300

    auto: Geo/App/Notch/NSScreen+Notch.swift

--- Geo/App/Notch/NSScreen+Notch.swift
+++ Geo/App/Notch/NSScreen+Notch.swift
@@ -28,3 +28,47 @@ extension NSScreen {
         physicalNotchSize ?? NSSize(width: 220, height: max(menubarHeight, 24))
     }
 }
+
+struct NotchMetrics {
+    let screenFrame: CGRect
+    let hasNotch: Bool
+    let notchSize: CGSize
+    let topInset: CGFloat
+
+    static let cornerRadius: CGFloat = 28
+    static let margin: CGFloat = 44
+    static let contentHeight: CGFloat = 290
+    static let maxDockWidth: CGFloat = 940
+
+    init(screen: NSScreen) {
+        screenFrame = screen.frame
+        hasNotch = screen.hasPhysicalNotch
+        notchSize = screen.effectiveNotchSize
+        topInset = screen.hasPhysicalNotch ? screen.effectiveNotchSize.height : screen.menubarHeight
+    }
+
+    var dockWidth: CGFloat { min(Self.maxDockWidth, screenFrame.width - 80) }
+    var dockHeight: CGFloat { topInset + Self.contentHeight }
+    var panelSize: CGSize { CGSize(width: dockWidth + Self.margin * 2, height: dockHeight + Self.margin) }
+
+    var panelFrame: CGRect {
+        CGRect(
+            x: screenFrame.midX - panelSize.width / 2,
+            y: screenFrame.maxY - panelSize.height,
+            width: panelSize.width,
+            height: panelSize.height
+        )
+    }
+
+    var hiddenHoverRect: CGRect {
+        let w: CGFloat = hasNotch ? notchSize.width + 40 : 280
+        let h: CGFloat = (hasNotch ? notchSize.height : topInset) + 20
+        return CGRect(x: screenFrame.midX - w / 2, y: screenFrame.maxY - h, width: w, height: h)
+    }
+
+    var expandedHoverRect: CGRect {
+        let w = dockWidth + 24
+        let h = dockHeight + 16
+        return CGRect(x: screenFrame.midX - w / 2, y: screenFrame.maxY - h, width: w, height: h)
+    }
+}
