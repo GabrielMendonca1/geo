5ce9a2624948f5367594c7e61259c159de5d4165
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-31 21:06:51 -0300

    auto: Geo/Features/Graph/UI/NodesPane.swift

--- Geo/Features/Graph/UI/NodesPane.swift
+++ Geo/Features/Graph/UI/NodesPane.swift
@@ -91,7 +91,7 @@ private struct ResizeDividerHandle: NSViewRepresentable {
     final class DividerNSView: NSView {
         var onDragChanged: ((CGFloat) -> Void)?
         var onDragEnded: (() -> Void)?
-        var isActive = true
+        var isActiveCheck: (() -> Bool)?
 
         private var trackingArea: NSTrackingArea?
         private var dragStartX: CGFloat?
