76a0305f9af63c31a3fda46f11a996f8baca39a4
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-01 15:07:52 -0300

    auto: Geo/Features/Graph/UI/GraphView.swift

--- Geo/Features/Graph/UI/GraphView.swift
+++ Geo/Features/Graph/UI/GraphView.swift
@@ -1033,7 +1033,6 @@ struct GraphView: View {
 
     private func renderCanvas(canvasContext: GraphicsContext, size: CGSize) {
         var ctx = canvasContext
-        let visibleSet = cachedVisibleSet
         let focused = cachedFocusedID
         let neighbors = cachedNeighbors
         let dimEverythingElse = focused != nil
