ff6bf4d5a1de4a7554e3f340a2d57961d2b1b881
GabrielMendonca1 <garimeipod@gmail.com>
2026-05-31 21:07:20 -0300

    auto: Geo/Shared/Navigation/UnifiedNavigationContainer.swift

--- Geo/Shared/Navigation/UnifiedNavigationContainer.swift
+++ Geo/Shared/Navigation/UnifiedNavigationContainer.swift
@@ -1,14 +1,5 @@
 import SwiftUI
 
-private struct TabIsActiveKey: EnvironmentKey { static let defaultValue = true }
-
-extension EnvironmentValues {
-    var tabIsActive: Bool {
-        get { self[TabIsActiveKey.self] }
-        set { self[TabIsActiveKey.self] = newValue }
-    }
-}
-
 struct UnifiedNavigationContainer: View {
     @Environment(\.tabRouter) private var tabRouter
     @State private var lruOrder: [AppTab] = []
