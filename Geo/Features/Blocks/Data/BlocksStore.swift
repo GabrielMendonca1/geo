4bab103c6565164faf6ded622474646f427c8dc9
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-03 18:25:42 -0300

    auto: Geo/Features/Blocks/Data/BlocksStore.swift

--- Geo/Features/Blocks/Data/BlocksStore.swift
+++ Geo/Features/Blocks/Data/BlocksStore.swift
@@ -724,6 +724,7 @@ class BlocksStore: ObservableObject {
         meta.status = self.markdownConverter.status(in: newMarkdown)
         meta.type = self.markdownConverter.type(in: newMarkdown)
         meta.layer = self.markdownConverter.layer(in: newMarkdown) ?? meta.layer
+        meta.tagName = MarkdownIndexingService.shared.extract(from: newMarkdown).tags.first
         let updated = Block(
             id: live.id,
             title: live.title,
