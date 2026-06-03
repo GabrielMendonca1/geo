84ec9299282589c798b1fab54811dbd56b3c7740
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-03 13:32:24 -0300

    auto: hermes-extensions/geo-http-tools/destructive.py

--- hermes-extensions/geo-http-tools/destructive.py
+++ hermes-extensions/geo-http-tools/destructive.py
@@ -25,7 +25,6 @@ from __future__ import annotations
 import asyncio
 import json
 import threading
-import time
 from typing import Any, Callable, Optional
 
 from .client import GeoAPIClient, GeoConflict, GeoError
