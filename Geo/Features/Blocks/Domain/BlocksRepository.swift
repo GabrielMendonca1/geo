4bef36805f7112a81e978c022c7fe34f9caa35db
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-03 19:29:01 -0300

    auto: Geo/Features/Blocks/Domain/BlocksRepository.swift

--- Geo/Features/Blocks/Domain/BlocksRepository.swift
+++ Geo/Features/Blocks/Domain/BlocksRepository.swift
@@ -21,6 +21,7 @@ protocol BlocksRepository: Sendable {
     func checkboxes(in blockId: String) async -> [BlockCheckbox]
     func toggleCheckbox(in blockId: String, lineNumber: Int) async throws
     func mutateFrontmatter(blockId: String, merge: [String: AnyCodableValue]) async throws -> Int
+    func linkToDay(blockId: String, dayId: String) async throws
     @MainActor func saveSync(id: String, markdown: String) -> Bool
 }
 
