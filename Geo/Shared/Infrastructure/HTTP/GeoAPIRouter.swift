6f8bc621205d388e9e2494e9aee5cc548268f88f
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-03 22:40:27 -0300

    auto: Geo/Shared/Infrastructure/HTTP/GeoAPIRouter.swift

--- Geo/Shared/Infrastructure/HTTP/GeoAPIRouter.swift
+++ Geo/Shared/Infrastructure/HTTP/GeoAPIRouter.swift
@@ -35,9 +35,7 @@ final class GeoAPIRouter: @unchecked Sendable {
             return tagCaller(.error(403, "insufficient scope: requires \(required.rawValue)"), token.callerId)
         }
 
-        let response = await GeoAPIRouter.$requestBrain.withValue(request.headers["x-geo-brain"]) {
-            await dispatch(request: request, token: token)
-        }
+        let response = await dispatch(request: request, token: token)
         return tagCaller(response, token.callerId)
     }
 
