bbd3e28f84ea5a31d5ae1c3c6bf368b2645cb232
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-03 21:08:00 -0300

    auto: Geo/Features/Graph/Data/GraphStore.swift

--- Geo/Features/Graph/Data/GraphStore.swift
+++ Geo/Features/Graph/Data/GraphStore.swift
@@ -113,7 +113,6 @@ final class GraphStore: ObservableObject {
             var hasher = Hasher()
             hasher.combine(block.title)
             hasher.combine(block.markdown)
-            hasher.combine(block.tagId)
             hasher.combine(block.metadata.tagName)
             hasher.combine(block.metadata.type)
             hasher.combine(block.metadata.layer)
