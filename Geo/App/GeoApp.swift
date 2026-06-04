241e89e59c1ea22f23a5a298b63c714e889a1271
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-04 00:02:04 -0300

    auto: Geo/App/GeoApp.swift

--- Geo/App/GeoApp.swift
+++ Geo/App/GeoApp.swift
@@ -55,7 +55,6 @@ final class AppContainer {
     let notchStateStore: NotchStateStore
     let notchWindowController: NotchWindowController
     let dayManager: DayManager
-    let mcpServer: MCPServer
     let httpServer: GeoHTTPServer
     let templateService: TemplateService
     let nanoHermesService: HermesStatusService
