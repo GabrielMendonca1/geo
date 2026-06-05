02fa6a45e78f5cb398984d5e63d717deaf633b2f
GabrielMendonca1 <garimeipod@gmail.com>
2026-06-05 14:48:48 -0300

    auto: Geo/App/GeoApp.swift

--- Geo/App/GeoApp.swift
+++ Geo/App/GeoApp.swift
@@ -335,11 +335,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         container.watcher.startWatching()
         Task { @MainActor in container.nanoHermesService.start() }
 
-        APITokenStore.shared.ensureBootstrapTokens()
-        do {
-            try container.httpServer.start()
-        } catch {
-            logger.error("Failed to start HTTP API server: \(error.localizedDescription)")
+        if FeatureFlags.httpServerEnabled {
+            APITokenStore.shared.ensureBootstrapTokens()
+            do {
+                try container.httpServer.start()
+            } catch {
+                logger.error("Failed to start HTTP API server: \(error.localizedDescription)")
+            }
         }
 
         PermissionRegistry.shared.refreshAll()
